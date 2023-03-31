273176c1c0e9e0169206ae3e0ff4622132485eae
vanor-cardozo <vanor.fc@gmail.com>
2023-03-31 16:42:14 -0300

    feat: Estrutura visual da página Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,29 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Callendário simbolizando a aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
